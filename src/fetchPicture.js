247fbaa0c1159a067ac251f135b0c266fd355ebc
Iuliia <iuliiaiavorskaia@gmail.com>
2022-06-15 13:21:50 +0300

    add task

--- src/fetchPicture.js
+++ src/fetchPicture.js
@@ -1,4 +1,7 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
+import { refs } from './index';
+
 axios.defaults.baseURL = 'https://pixabay.com';
 const API_KEY = '28000983-d0b2a085634fa0bb803984db3';
 const per_page = 40;
@@ -21,6 +24,17 @@ async function onFetchPicture(name, isNewRequest) {
   });
   if (!response) {
     return;
+  }
+
+  const totalPage = Math.ceil(response.data.totalHits / per_page);
+
+  if (page === totalPage) {
+    refs.btnLoadMore.classList.add('is-hidden');
+    Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  } else {
+    refs.btnLoadMore.classList.remove('is-hidden');
   }
   page += 1;
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { getMarkupCards } from './getMarkupCards';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from 'simplelightbox';
 
-const refs = {
+export const refs = {
   searchFormEl: document.querySelector('#search-form'),
   galleryEl: document.querySelector('.gallery'),
   btnLoadMore: document.querySelector('.load-more'),
@@ -14,65 +14,50 @@ const refs = {
 refs.searchFormEl.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', addPageMore);
 
-async function selectGallery(isNewRequest, page) {
+let gallery = new SimpleLightbox('.gallery a', {});
+
+async function selectGallery(isNewRequest) {
   try {
     const valueText = refs.searchFormEl.elements.searchQuery.value.trim();
 
     if (!valueText) {
       return;
     }
+
     const response = await onFetchPicture(valueText, isNewRequest);
-    if (isNewRequest) {
+
+    if (isNewRequest && response.totalHits !== 0) {
       page = 1;
       Notify.success(`Hooray! We found ${response.totalHits} images.`);
     }
     getMarkupCards(refs.galleryEl, response);
-    let gallery = new SimpleLightbox('.gallery a', {});
+    gallery.refresh();
     if (response.total === 0) {
       Notify.warning(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      showBtnLoadMore(false);
     }
-    function isLastPage(response) {
-      const currentPage = page;
-      const totalPages = Math.ceil(response.totalHits / 39);
-
-      return currentPage === totalPages ? false : true;
-    }
-    if (isLastPage(response)) {
-      refs.btnLoadMore.classList.add('is-hidden');
-      Notify.warning('OK');
-    }
-    // if (page === Math.ceil(response.totalHits / response.per_page)) {
-    //   refs.btnLoadMore.classList.add('is-hidden');
-    //   Notify.warning('OK');
-    // }
   } catch (error) {
     console.log(error);
   }
 }
-// function isLastPage(response) {
-//   const currentPage = page;
-//   const totalPages = Math.ceil(response.data.totalHits / per_page);
-
-//   return currentPage === totalPages ? false : true;
-// }
 
 function onSearch(evt) {
   evt.preventDefault();
   refs.galleryEl.innerHTML = '';
   selectGallery(true);
-  showBtnLoadMore();
+  showBtnLoadMore(false);
 }
 function addPageMore() {
-  toHideBtnLoadMore();
+  showBtnLoadMore(false);
   selectGallery(false);
 }
 
-function toHideBtnLoadMore() {
-  refs.btnLoadMore.classList.add('is-hidden');
-  refs.btnLoadMore.classList.remove('is-hidden');
-}
-function showBtnLoadMore() {
-  refs.btnLoadMore.classList.remove('is-hidden');
+function showBtnLoadMore(flag) {
+  if (flag) {
+    refs.btnLoadMore.classList.remove('is-hidden');
+  } else {
+    refs.btnLoadMore.classList.add('is-hidden');
+  }
 }
